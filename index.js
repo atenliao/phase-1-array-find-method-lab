dcd4d5f574b7230f4ae601dc6bacdf7d57a3d8b1
Zhongming <atenliao@gmail.com>
2023-03-24 23:15:56 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+const superbowlWin = ( arr) => {
+     const getResult = arr.find((Input)=>{
+         return Input.result === 'W'
+    })
+    if(getResult){
+        return getResult.year;
+    }else if(getResult === undefined){
+        return undefined
+    }
+   
+}
